50b99ca004907cb35f1affc87d243069220a2e36
rdesmond21 <128184517+Dezz-x@users.noreply.github.com>
2024-03-07 22:31:29 -0500

    Moved back to top btn to footer

--- script.js
+++ script.js
@@ -4,13 +4,3 @@ function topFunction (){
     document.body.scrollTo({top: 0, behavior:"smooth"});
     document.documentElement.scrollTo({top: 0, behavior: "smooth"});
 }
-
-window.onscroll = function(){scrollFunction()};
-
-function scrollFunction (){
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
-        scrollToTop.style.display = "block";
-    }else {
-        scrollToTop.style.display= "none";
-    }
-}
